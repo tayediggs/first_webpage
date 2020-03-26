2ccc1a39c883813c80cd5d6f7611fb2a97ff42dd
tayediggs <61007777+tayediggs@users.noreply.github.com>
2020-03-26 21:57:40 +0100

    taye js task1
    java task

---
+++ taye js task 1/scripts.js
@@ -0,0 +1,9 @@
+let name = "Abiola Taiwo";
+let courses = ["html", "css", "javascript"];
+
+console.log(name, courses);
+
+let n = courses.length;
+for (let i = 1; i <= 200; 1++) {
+    if (i % 2 !== 0) console.log(i);
+}
